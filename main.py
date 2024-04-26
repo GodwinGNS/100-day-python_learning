2a89f77706b58b21189337b0ca7fea546a33a0c7
GodwinGNS <122378339+GodwinGNS@users.noreply.github.com>
2024-04-26 10:39:32 +0000

    update typo

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 # Write your code below this line 👇
-print('my start up project in python')
+#print('my start up project in python')
+
+#print ('hellow\n how are you?')
+
+
+#print("hellow world\nhello Godwin")
+
+#print("hello" +" "+"GODWINAA")
+
+
+#print ("Hello " + input("what is your name? "))
+
+
+print("welcome to the Band Name Genrator.\n")
+city = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
+
+
+
+
+
